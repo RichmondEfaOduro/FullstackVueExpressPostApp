d2466b4fd52a7d8f28ba630dc58b1585e36e6af7
Richmond Oduro <richmond.eo@gmail.com>
2019-12-06 11:22:25 +0100

    added static folder and prep. SPA

---
+++ client/src/PostService.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const url = 'api/posts/';
+
+class PostService {
+    //GET post
+    static getPosts () {
+        return new Promise (async (resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(post =>  ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                );
+            }catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    //CREATE post 
+    static insertPost (text) {
+        return axios.post(url, { text });
+    }
+
+    //DELETE post 
+    static deletePost (id) {
+        return axios.delete(`${url}${id}`)
+    }
+}
+
+export default PostService;
